706de0dcf36f1c52602ee678e807964416743f92
Engineous <106715691+Arjun-Sur@users.noreply.github.com>
2022-06-10 10:55:41 -0700

    Created simple React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello World</h1>
+        <p>This is a simple React App</p>
+      </div>
+    )
+  }
+}
+
+export default App
