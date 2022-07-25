b96b03ed4a987fceeace0ae3eae2d130061ce6b9
Arjun-Sur <106715691+Arjun-Sur@users.noreply.github.com>
2022-07-25 11:00:29 -0700

    Added pan and zoom and removed excess cells

--- client/src/viewer/leadtools/LeadtoolsViewer.js
+++ client/src/viewer/leadtools/LeadtoolsViewer.js
@@ -14,14 +14,8 @@ const LeadtoolsViewer = ({ files, select }) => {
             return;
         }
 
-        // Create the medical viewer control, and specify the number or rows and columns.
-        var viewer = new lt.Controls.Medical.MedicalViewer(imageViewerDiv, 2, 2);
-
-        // [optional] Update the splitter size so it become thick and easy to move.
-        viewer.get_gridLayout().set_splitterSize(7);
-
-        // Create a cell name var
-        var cellName = "MedicalCell" + Date.now();
+        // Create the medical viewer control
+        var viewer = new lt.Controls.Medical.MedicalViewer(imageViewerDiv, 1, 1);
 
         // Create a cell. It will contain an image or a series of images, based on how many Frames are added (see below for more details).
         var cell = new lt.Controls.Medical.Cell(viewer, viewer.get_divId(), 1, 1);
@@ -32,7 +26,6 @@ const LeadtoolsViewer = ({ files, select }) => {
         // Add the cell to the viewer.
         viewer.layout.get_items().add(cell);
 
-        // [optional] Select the cell (it can also be selected by clicking on it.)
         cell.set_selected(true);
 
         Array.from(files.values()).forEach(file => {
@@ -45,7 +38,6 @@ const LeadtoolsViewer = ({ files, select }) => {
             var mrtiInfo = new lt.Controls.Medical.MRTIImage();
 
             // The image dpi.
-            //mrtiInfo.fullDpi = lt.LeadSizeD.create(150, 150);
             mrtiInfo.fullDpi = lt.LeadSizeD.create(width, height);
 
             // the tile size, recommended value is 256
@@ -70,8 +62,8 @@ const LeadtoolsViewer = ({ files, select }) => {
             }
 
             // the image width and height.
-            cellFrame.set_width(width/*mrtiInfo.resolutions[0].width*/);
-            cellFrame.set_height(height/*mrtiInfo.resolutions[0].height*/);
+            cellFrame.set_width(width);
+            cellFrame.set_height(height);
 
             // the image full size.
             mrtiInfo.fullSize = lt.LeadSizeD.create(
@@ -89,8 +81,8 @@ const LeadtoolsViewer = ({ files, select }) => {
             // now we need to set the information for the image so we can do window level.
             var imageInfo = new lt.Controls.Medical.DICOMImageInformation();
 
-            imageInfo.width = file.data.elements.Columns;
-            imageInfo.height = file.data.elements.Rows;
+            imageInfo.width = width;
+            imageInfo.height = height;
 
             imageInfo.photometricInterpretation = file.data.elements.PhotometricInterpretation;
             imageInfo.minValue = 0;
@@ -103,20 +95,32 @@ const LeadtoolsViewer = ({ files, select }) => {
             cellFrame.set_information(imageInfo);
         });
 
-        // [optional] Add an action that allows the user to move the loaded image using either the mouse or by touch and drag. we are adding an offset action.
-        cell.setCommand(1, new lt.Controls.Medical.OffsetAction());
-
-        // [optional] Add an action that allows the user to do window level on the image.
-        cell.setCommand(2, new lt.Controls.Medical.WindowLevelAction());
-
-        // [optional] Run the action. Now if the user clicks or touches the image and drags it, the image will move correspondingly.
-        cell.runCommand(2);
-
+        // Creating the window level action
         var windowLevel = new lt.Controls.Medical.WindowLevelAction();
-        cell.setCommand(0, windowLevel);
-        cell.runCommand(0);
+        // Setting the action to left mouse button.
+        windowLevel.button = lt.Controls.MouseButtons.left; 
+        // Setting the command
+        cell.setCommand(0, windowLevel); 
+        // Running it
+        cell.runCommand(0); 
 
-        viewer.explode(cell, true);
+        // Creating the scale action
+        var scale = new lt.Controls.Medical.ScaleAction(); 
+        // Setting the button to middle mouse button.
+        scale.button = lt.Controls.MouseButtons.middle; 
+        // Setting the command
+        cell.setCommand(0, scale);
+        // Running it 
+        cell.runCommand(0); 
+
+        // Creating the pan action
+        var offset = new lt.Controls.Medical.OffsetAction(); 
+        // Setting the button to right mouse button.
+        offset.button = lt.Controls.MouseButtons.right; 
+        // Setting the command
+        cell.setCommand(0, offset); 
+        // Running it
+        cell.runCommand(0); 
     }, []);
 
     select((text) => {
